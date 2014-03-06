76450e68aee0eaf3a736009ceaeada91731175b4
ssjylsg <ssjylsg@gmail.comssjylsg@gmail.com>
2014-03-06 15:07:12 +0800

    update

--- OpenLayers/lib/OpenLayers/Layer/TMS_PGIS.js
+++ OpenLayers/lib/OpenLayers/Layer/TMS_PGIS.js
@@ -182,8 +182,8 @@ OpenLayers.Layer.TMS_PGIS = OpenLayers.Class(OpenLayers.Layer.Grid, {
         var x = (Math.round(Math.abs((center.lon - originTileX) / (res * this.tileSize.w)))); 
         var y = (Math.round(Math.abs((originTileY - center.lat) / (res * this.tileSize.h)))); 
 		 if (x == 0 || y == 0) return null;
-        var z = this.map.getZoom();        		 
-	   return "http://10.173.2.20/PGIS_S_TileMapServer/Maps/V/EzMap?Service=getImage&Type=RGB&ZoomOffset=0&V=0.3&Col=" +x+ "&Row="+y+"&Zoom="+z;
+        var z = this.map.getZoom(); 
+	    return this.url +  "/EzMap?Service=getImage&Type=RGB&ZoomOffset=0&V=0.3&Col=" +x+ "&Row="+y+"&Zoom="+z;
     },
 
     /** 
